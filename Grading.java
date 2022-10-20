29666348ac99533c438d663ab194dfaaa09ebd23
Bahar Demirkan <105504150+BaharDemirkan@users.noreply.github.com>
2022-10-20 14:48:41 +0300

    Create Grading.java
    First assignment

---
+++ Grading.java
@@ -0,0 +1,36 @@
+package labs;
+
+import java.util.Scanner;
+
+public class Grading {
+    public static void main(String[] args) {
+        System.out.println("Please enter the overall score:");
+
+        Scanner score = new Scanner(System.in);
+
+        double grade =score.nextDouble();
+
+        if (grade<50.0 && grade>=0.0){
+            System.out.println("You failed, FF");
+        } else if (grade<60.0){
+            System.out.println("You passed, DD");
+        } else if (grade<65.0){
+            System.out.println("You passed, DC");
+        } else if (grade<70.0){
+            System.out.println("You passed, CC");
+        } else if (grade<75.0){
+            System.out.println("You passed, CB");
+        } else if (grade<80.0){
+            System.out.println("You passed, BB");
+        } else if (grade<85.0){
+            System.out.println("You passed, BA");
+        } else if (grade<90.0){
+            System.out.println("You passed, AB");
+        } else if (grade<=100.0){
+            System.out.println("You passed, AA");
+        } else {
+            System.out.println("Invalid score, program terminates.");
+        }
+
+    }
+}
